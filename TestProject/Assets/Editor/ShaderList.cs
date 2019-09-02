05499e186b23454e44d1deba4b60ee41fc8875bf
孟昱成 <767644624@qq.com>
2019-09-02 18:33:25 +0800

    初始化仓库

---
+++ TestProject/Assets/Editor/ShaderList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderList : MonoBehaviour
+{
+    private static ShaderVariantCollection svc = new ShaderVariantCollection();
+    
+    
+}
